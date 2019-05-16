ca749572d404c89a315475c4eb0fd2eb49bad1ac
edgararuiz <edgararuiz@gmail.com>
2019-05-15 21:07:02 -0500

    switches output to plain print, simplifies structure routine

--- R/gregorian_date.R
+++ R/gregorian_date.R
@@ -1,29 +1,11 @@
 #' @export
 gregorian_date <- function(year_no, month_no, day_no, bce) {
-  # ------------------- Validations ----------------
-  ai <- as.logical(lapply(c(year_no, month_no, day_no), is_integer))
-  if(!all(ai)) stop("Year, month and day must be an integer number")
-  ai <- as.logical(lapply(c(year_no, month_no, day_no), function(x) x > 0)) 
-  if(!all(ai)) stop("Year, month and day must be a positive number")
-  bce <- as.logical(bce)
-  if(month_no > 12) stop("Month entry not valid. Valid numbers are between 1 and 12")
-  if(day_no > 31) stop("Day entry not valid. Valid numbers are between 1 and 31")
-  # -------------------------------------------------
-  dn <- day_name(year_no, month_no, day_no, bce)
-  yr_astro <- ifelse(bce, year_no - 1, year_no)
-  astro <- paste0(
-    ifelse(yr_astro > 0 && bce, "-", ""), 
-    yr_astro, "-" ,
-    ifelse(nchar(month_no) == 1, "0", ""), month_no, "-", 
-    ifelse(nchar(day_no) == 1, "0", ""), day_no
-    )
-  output <- paste0(
-    dn$name, " ", 
-    month.name[month_no], " ", 
-    day_no, ", ", 
-    year_no," ", 
-    ifelse(bce, "BCE", "CE")
-    )
+  
+  validate_gregorian(year_no, month_no, day_no, bce)
+  dn     <- day_name(year_no, month_no, day_no, bce)
+  astro  <- atronomical_date(year_no, month_no, day_no, bce)
+  output <- full_date(year_no, month_no, day_no, bce, dn$name)
+  
   structure(
     list(
       year = year_no,
@@ -36,12 +18,44 @@ gregorian_date <- function(year_no, month_no, day_no, bce) {
     ),
     class = "gregorian_date"
   )
+  
 }
 
 setOldClass("gregorian_date")
 
 #' @export
 print.gregorian_date <- function(x, ...) {
-  cat(x$full_date)
+  print(x$full_date)
   invisible(x)
+}
+
+validate_gregorian <- function(year_no, month_no, day_no, bce) {
+  c_date <- c(year_no, month_no, day_no)
+  ai <- as.logical(lapply(c_date, is_integer))
+  if(!all(ai)) stop("Year, month and day must be an integer number")
+  ai <- as.logical(lapply(c_date, function(x) x > 0)) 
+  if(!all(ai)) stop("Year, month and day must be a positive number")
+  bce <- as.logical(bce)
+  if(month_no > 12) stop("Month entry not valid. Valid numbers are between 1 and 12")
+  if(day_no > 31) stop("Day entry not valid. Valid numbers are between 1 and 31")
+}
+
+atronomical_date <- function(year_no, month_no, day_no, bce) {
+  yr_astro <- ifelse(bce, year_no - 1, year_no)
+  paste0(
+    ifelse(yr_astro > 0 && bce, "-", ""), 
+    yr_astro, "-" ,
+    ifelse(nchar(month_no) == 1, "0", ""), month_no, "-", 
+    ifelse(nchar(day_no) == 1, "0", ""), day_no
+  )
+}
+
+full_date <- function(year_no, month_no, day_no, bce, day_name) {
+  paste0(
+    day_name, " ", 
+    month.name[month_no], " ", 
+    day_no, ", ", 
+    year_no," ", 
+    ifelse(bce, "BCE", "CE")
+  )
 }
--- tests/testthat/test-gregorian.R
+++ tests/testthat/test-gregorian.R
@@ -3,6 +3,6 @@ context("gregorian")
 test_that("Special diff_days cases", {
   expect_equal(
     capture.output(as_gregorian("2012-1-1")),
-    "Sunday January 1, 2012 CE"
+    "[1] \"Sunday January 1, 2012 CE\""
   )
 })
